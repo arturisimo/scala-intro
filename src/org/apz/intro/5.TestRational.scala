2191805474b250730fe7f37ff722a5e07db9105f
arturisimo <arturisimo@gmail.com>
2018-07-10 20:28:47 +0200

    classes

--- src/org/apz/intro/5.TestRational.scala
+++ src/org/apz/intro/5.TestRational.scala
@@ -1,5 +1,6 @@
 package org.apz.intro
 
+
 object TestRational extends App {
   
   import org.apz.intro.classes.Rational
@@ -27,5 +28,41 @@ object TestRational extends App {
     //test equals
     assert( Rational(2, 3) == Rational(4, 6))
   
+    //test equals
+    
+    val r5 = Rational( 1, 5)
+    val r6 = Rational( 2, 7 )
   
+    assert( r5 + r6 == Rational(17, 35))
+    assert( r5 - r6 == Rational(-3, 35))
+    assert( r6 - r5 == Rational(3, 35))
+    assert( r5 * r6 == Rational(2, 35))
+    assert( r5 / r6 == Rational(7, 10) )
+    assert( r6 / r5 == Rational(10, 7) )
+    
+    assert( r5 + r6 == Rational(1, 3))
+  
+    
+    assert( r5 + 5 == Rational(6, 5))
+    
+    
+    assert( 2 * r2 == Rational(4, 7 ))
+    
+    //unapply
+    var Rational(n,d) = r * r2
+    assert(n == 3 && d == 35 )
+    
+    r2 match {
+      case Rational(2,v) => assert (v==7)
+      case _ => assert(false)
+    }
+    
+    
+    //sort compare
+    /*
+    val l = List (Rational( -3, 5 ), Rational(6, 6), Rational(300, 500 ))
+    assert (l.sorted() = List (Rational( -3, 5 ), Rational(6, 6 ), Rational(300, 500 )))*/
+    
+    
+    
 }
--- src/org/apz/intro/classes/Rational.scala
+++ src/org/apz/intro/classes/Rational.scala
@@ -7,32 +7,51 @@ package org.apz.intro.classes
  * de esta manera los metodos de Rational son estaticos
  */
 object Rational {
+  
   def apply(n: Int, d: Int) = new Rational(n,d)
+  
+  def unapply(r: Rational): Option[(Int, Int)] = {
+    if(r.d == 0) None
+    else Some((r.n, r.d))
+  }
+  
+  implicit class IntOps (n: Int) {
+  
+    def + (r: Rational ) = Rational( n, 1 ) + r
+    def - (r: Rational ) = Rational( n, 1 ) - r
+    def * (r: Rational ) = Rational( n, 1 ) * r
+    def / (r: Rational ) = Rational( n, 1 ) / r
+  
+  }
+  
 }
 
 /**
  * Clase que representa a un numero racional:  n numerador - d denominador
  * n_ y d_ son atributos privados de la clase y solo existen en el ambito de la clase
  * n y d son los atributos publicos
+ * 
+ * implementa el Trait Ordered para que se pueda ordenar por el metodo sort. Hay que añadir
+ * el metodo compare
  */
-class Rational private (n_ : Int, d_ : Int) {
+class Rational private (n_ : Int, d_ : Int) extends Ordered[Rational] {
   
   //obtener el valor normalizado
   private def normalizar (n: Int, d:Int): (Int, Int) = {
     
-    //algoritmo de euclides para obtener el max comun divisor
-    def mcd(n: Int, d:Int) : Int = {
-        var resto = n%d
-        if (resto == 0) d.abs
-        else mcd(d, resto)
-    }
-    
-    if (d==0) throw new IllegalArgumentException(d.toString())
-    else if(n==0) {
-       (0,1) 
-    } else {
-      ( (n/mcd(n,d)).abs * (if (n * d > 0 ) 1 else -1) , (d/mcd(n,d)).abs)
-    }
+      //algoritmo de euclides para obtener el max comun divisor
+      def mcd(n: Int, d:Int) : Int = {
+          var resto = n%d
+          if (resto == 0) d.abs
+          else mcd(d, resto)
+      }
+      
+      if (d==0) throw new IllegalArgumentException(d.toString())
+      else if(n==0) {
+         (0,1) 
+      } else {
+        ( (n/mcd(n,d)).abs * (if (n * d > 0 ) 1 else -1) , (d/mcd(n,d)).abs)
+      }
   }
   
   //sobrescribimos metodo toString
@@ -45,6 +64,12 @@ class Rational private (n_ : Int, d_ : Int) {
     }
   }
   
+  override def compare(r2: Rational) : Int = {
+     if(n * r2.d < d * r2.n) -1
+      else if(n * r2.d == d * r2.n) 0
+      else 1
+  }
+  
   override def hashCode() : Int = {
     n * d
   }
@@ -53,6 +78,17 @@ class Rational private (n_ : Int, d_ : Int) {
   val (n,d) = normalizar(n_, d_)
   println(this.toString())
   
+  def + (r2:Rational) : Rational = Rational ((n * r2.d + r2.n * d), (d * r2.d)) 
+  def - (r2:Rational) : Rational = Rational ((n * r2.d - d * r2.n), (d * r2.d))
+  def * (r2:Rational) : Rational = Rational (n*r2.n, d * r2.d)
+  def / (r2:Rational) : Rational = Rational (n*r2.d, d * r2.n)
+  
+  def + (i:Int) : Rational = this + Rational(i,1)
+  def - (i:Int) : Rational = this - Rational(i,1)
+  def * (i:Int) : Rational = this * Rational(i,1)
+  def / (i:Int) : Rational = this / Rational(i,1)
+  
+  def incr(i:Int) : Rational = this * Rational(i,1)
   
   
 }
