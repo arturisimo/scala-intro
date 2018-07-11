188bb028dfb85802e4c656a801261e2e58e7815f
arturisimo <arturisimo@gmail.com>
2018-07-11 19:39:41 +0200

    repaso

--- src/org/apz/intro/0.HelloScala.scala
+++ src/org/apz/intro/0.HelloScala.scala
@@ -6,16 +6,31 @@ import org.apz.intro.classes.StringUtils._
 /**
  * Scala (SCALable LAnguaje) es un superset de Java, funciona en la JVM.
  * Scala es mas expresivo (menos línes de codigo) y tiene procesamiento implicito 
+ * Es un lenguaje compilado scalac, compilador de Scala, genera el .class
  * Scala soporta programacion imperativa, POO y funcional 
  * 
- * Se basa en: funciones puras y datos inmutables
+ * La programacion funcional se adopta paa programación paralela y distribuida (multicores, GPUs, clusters) 
+ *  
+ * Scala se basa en: funciones puras y variables inmutables
+ * El código de Scala se escribe en bloques, expresiones que siempre devuelven un valor
+ * Las funciones son "ciudadanos de primer orden" se consideran como una variable, se pueden pasar como parámetro
+ * Se ha de procurar que sean funciones "puras" que no tienen "side effects" no cambia el valor de las variables fuera 
+ * del ámbito de la funcion
  *
+ * 
+ * Tipos en Scala - Byte / Short / Int /Long / Double / Float / Char / String / Boolean
+ * Tipos especiales 
+ * - Unit vacio / - Any todos extienden de Any  
+ * 
  */
 object HelloScala {
   
   def main(args: Array[String]) {
       
-      println("Hello, Scala!")
+      //val (value) – immutable
+      val string : String = "Scala"
+    
+      println(s"Hello, $string!")// uso de parametros en String 
       
       //Cuando hacemos una llamada a una función con cero o un parámetro, 
       //ésta puede ser llamada sin utilizar el punto y los paréntesis. 
@@ -26,7 +41,8 @@ object HelloScala {
       
       myList foreach println
       
-      val hi = "Hello"
+      //var (variable) mutable
+      var hi = "Hello" //no decimos que es un String. Scala hace inferencia de tipos
       
       println(hi length)
       println(hi.length())
@@ -58,8 +74,11 @@ object HelloScala {
       
       
       //uso de clase impliticta
-      val string : String = "HAL"
-      println(string.increment)
+      val hal : String = "HAL"
+      println(hal.increment)
+      
+      //Any: todos los elementos de esta coleccion extienden de Any
+      val randomlist = List("iFruit", 3, "Ronin", 5.2)//List[Any] = List(iFruit, 3, Ronin, 5.2)
       
       
   }
