5348dcb6333f02ade5660eda19d465e0a88dd035
Arturo Pérez <arturo.perez@baratznet.local>
2018-07-12 12:53:53 +0200

    limpia

--- src/org/apz/intro/classes/StringIncrementado.scala
+++
@@ -1,6 +0,0 @@
-package org.apz.intro.classes
-
-implicit class StringIncrementado( s: String ) {
-	//metodo sin argumentos. Emplea una funcion lambda => 
-	def increment() = s.map ( c => (c+1).toChar)
-}
